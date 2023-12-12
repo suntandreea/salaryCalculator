b3ef316214954c4c39b36c28d75769c0fd71286b
Andreea <suntandreea@protonmail.com>
2023-12-12 19:48:49 +0200

    creating classes

---
+++ src/main/java/org/example/TextBox.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class TextBox {
+    public String text; // Field
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void clear() {
+        text = "";
+    }
+}
