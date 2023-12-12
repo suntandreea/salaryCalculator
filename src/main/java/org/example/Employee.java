749f1aec23f091d962a80d04f9040bfc899c000b
Andreea <suntandreea@protonmail.com>
2023-12-12 20:55:14 +0200

    getters and setters

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,10 +1,30 @@
 package org.example;
 
 public class Employee {
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
     public int calculateWage(int extraHours) {
-        return baseSalary + (hourlyRate * extraHours);
+        return baseSalary + (getHourlyRate() * extraHours);
+    }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less.");
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly rate cannot be 0 or negative.");
+        this.hourlyRate = hourlyRate;
     }
 }
