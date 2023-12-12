7e15cb1ce80557d00a0b55c2c412785a933fdc1f
Andreea <suntandreea@protonmail.com>
2023-12-12 21:59:46 +0200

    abstraction & reducing coupling

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,14 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        var employee = new Employee();
-        employee.setBaseSalary(50_000);
-        employee.setHourlyRate(20);
-        int wage = employee.calculateWage(10);
-        System.out.println(wage);
+//        var employee = new Employee();
+//        employee.setBaseSalary(50_000);
+//        employee.setHourlyRate(20);
+//        int wage = employee.calculateWage(10);
+//        System.out.println(wage);
+
+        var browser = new Browser();
+        browser.navigate("asdasd");
     }
 
 }
